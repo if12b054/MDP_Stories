141243c63e6bd5187327ac49d186e103ebf562eb
if12b103 <georg.huszar@gmail.com>
2014-06-03 00:09:30 +0200

    fixed width for Buttons in all Activities

--- StoryTellers/src/com/example/storytellers/RoomActivity.java
+++ StoryTellers/src/com/example/storytellers/RoomActivity.java
@@ -1,10 +1,12 @@
 package com.example.storytellers;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -37,6 +39,9 @@ public class RoomActivity extends Activity {
 		} else {
 			story.append(sentence.getText().toString());
 			sentence.setText("");
+			InputMethodManager imm = (InputMethodManager) getSystemService(
+				      Context.INPUT_METHOD_SERVICE);
+			imm.hideSoftInputFromWindow(sentence.getWindowToken(), 0);
 		}
 	}
 
